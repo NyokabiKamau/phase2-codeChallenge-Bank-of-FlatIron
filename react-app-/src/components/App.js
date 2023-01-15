82eb01bae3236d6e93a6763fdce23de9be2cdc2e
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-01-15 18:16:34 +0300

    removed the table tag

--- react-app-/src/components/App.js
+++ react-app-/src/components/App.js
@@ -15,7 +15,7 @@ function App() {
 
     return (
         <div className="rootDiv">
-            <div><Navbar /></div>
+            <Navbar />
             <Transactions transactions={transaction} />
         </div>
     )
--- react-app-/src/components/TransactionItem.js
+++ react-app-/src/components/TransactionItem.js
@@ -2,14 +2,12 @@ import React from "react";
 
 function TransactionItem(props) {
     return (
-        <table>
-            <tr>
-               <td>{props.date}</td>
-               <td>{props.description}</td>
-               <td>{props.category}</td>
-               <td>{props.amount}</td>
-            </tr>
-        </table>
+        <tr>
+            <td>{props.date}</td>
+            <td>{props.description}</td>
+            <td>{props.category}</td>
+            <td>{props.amount}</td>
+        </tr>
     )
 }
 
