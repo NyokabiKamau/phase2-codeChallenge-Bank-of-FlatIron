a16ca0f480a77bc3512139769ead0785201b3b54
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-01-15 16:53:48 +0300

    changed the npm start command back to original and succefully fetched data

--- react-app-/src/components/App.js
+++ react-app-/src/components/App.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React, {useEffect} from "react"
 import Navbar from "./Navbar"
 
+const API = "http://localhost:8001/transactions"
+
 function App() {
+    useEffect(() => {
+        fetch(API)
+        .then((response) => response.json())
+        .then((transaction) => console.log(transaction))
+    })
+
     return (
         <div className="rootDiv">
             <Navbar />
