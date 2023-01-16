343e788941274e9518708fef5b17a537d9bbf407
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-01-17 00:53:38 +0300

    removed the react-app-, now using bank-of-flat-iron

--- bank-of-flatiron/src/components/TransactionForm.js
+++ bank-of-flatiron/src/components/TransactionForm.js
@@ -1,12 +1,30 @@
-function TransactionForm() {
+import React, {useState} from "react"
+
+function TransactionForm({prop}) {
+    const [formItem, setFormItem] = useState({
+        date:"", 
+        description:"",
+        category:"", 
+        amount:0, 
+    })
+
+    function addTransaction(e) {
+        e.preventDefault()
+        prop(formItem)
+    } 
+
+    function handleChange(e) {
+        setFormItem({...formItem, [e.target.name]:[e.target.value]})
+    }
+
     return (
-        <form id="form-item">
+        <form onChange={handleChange} id="form-item" onSubmit={addTransaction} >
             <div className="form-inputs">
                 <label htmlFor="date">Date</label>
-                <input type="date" id="date"/>
-                <input type="text" placeholder="Description" className="text-input"/>
-                <input type="text" placeholder="Category" className="text-input"/>
-                <input type="number"/>
+                <input value={formItem.date} name="date" type="date" id="date"/>
+                <input value={formItem.description} name="description" type="text" placeholder="Description" className="text-input"/>
+                <input value={formItem.category} name="category" type="text" placeholder="Category" className="text-input"/>
+                <input name="amount" value={formItem.amount} type="number"/>
             </div>
             <button >Add Transaction</button>
         </form>
